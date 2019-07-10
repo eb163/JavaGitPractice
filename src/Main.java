c269df8011eae314367c8d60c297c68e3e3f740c
realEricBrun <ericbrun350@att.net>
2019-07-10 08:34:00 -0500

    Added a message to demonstrate merging master with dev

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world! This is the Main file!");
+		System.out.println("This message demonstrates rebasing and merging!");
 	}
 
 }
