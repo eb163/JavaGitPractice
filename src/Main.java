ad53d14291d6f3935e10ae2426a01ec467f551b6
realEricBrun <ericbrun350@att.net>
2019-07-10 13:32:52 -0500

    finalized Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//BranchB b = new BranchB();
+		//BranchC c = new BranchC();
 		System.out.println("Hello world! This is the Main file!");
 		System.out.println("This message demonstrates rebasing and merging!");
-		System.out.print("Incoming message from Branch B: "); BranchB.print();
+		System.out.print("Incoming message from Branch B: "); BranchB.print(); //b.print();
 		System.out.println("This is a message from Branch A!");
-		System.out.print("Incoming message from Branch C: "); BranchC.print();
+		System.out.print("Incoming message from Branch C: "); BranchC.print(); //c.print();
 	}
 
 }
