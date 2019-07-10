7558966bdd72e71e8ac98cb530eef84e8128dedf
realEricBrun <ericbrun350@att.net>
2019-07-10 08:47:12 -0500

    Edited Main to utilize BranchB class

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world! This is the Main file!");
 		System.out.println("This message demonstrates rebasing and merging!");
+		System.out.print("Incoming message from Branch B: "); BranchB.print();
 	}
 
 }
